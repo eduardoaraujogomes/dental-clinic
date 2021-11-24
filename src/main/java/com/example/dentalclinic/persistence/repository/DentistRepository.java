f948f50291108655239aedb4b2d2b34c9f81c4f7
Eduardo de Araújo Gomes Filho <73198111+eduardoaraujogomes@users.noreply.github.com>
2021-11-24 16:33:54 -0300

    Created the repositories for Address, Dentist and Patient

---
+++ src/main/java/com/example/dentalclinic/persistence/repository/DentistRepository.java
@@ -0,0 +1,6 @@
+package com.example.dentalclinic.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DentistRepository extends JpaRepository<DentistRepository, Integer> {
+}
